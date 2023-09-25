68696eb036c9113d53d67012e484b696176c83b4
toyman640 <falakoadebayor@gmail.com>
2023-09-25 16:26:41 +0100

    Set up mofdel and relations

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'stimulus-rails'
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem 'jbuilder'
 gem 'rubocop', '>= 1.0', '< 2.0'
+gem 'cancancan'
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :categories, foreign_key: :user_id
+  has_many :dealings, foreign_key: :user_id
 end
